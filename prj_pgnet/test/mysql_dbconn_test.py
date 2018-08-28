47cd7440a8ff3d41a7e0375f8ef0697967658d8a
thanksLee <dmzone75@naver.com>
2018-08-29 00:40:31 +0900

    추가

--- prj_pgnet/test/mysql_dbconn_test.py
+++ prj_pgnet/test/mysql_dbconn_test.py
@@ -3,7 +3,7 @@ import MySQLdb as mysql
 db = mysql.connect(host="localhost", user="devpythontest", passwd="Devpythontest0!", db="dev_pythontest_db")
 
 cur = db.cursor()
-cur.execute("SELECT * FROM user")
+cur.execute("SELECT * FROM users")
 print(cur.fetchall())
 print("-----------------------------------------")
 
